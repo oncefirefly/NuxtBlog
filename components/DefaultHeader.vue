d5c2011a9f5a5b8a8ba669f5e176d72cd741a2f4
oncefirefly <oncefirefly@gmail.com>
2023-08-02 21:02:21 +0300

    feat: Header component added

---
+++ components/DefaultHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="w-100 bg-slate-700">
+    <h1
+      class="max-w-7xl px-10 h-14 mx-auto my-0 0 flex items-center text-white font-black "
+    >
+      QTIM-test Blog
+    </h1>
+  </header>
+</template>
