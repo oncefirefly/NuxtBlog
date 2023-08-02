15dbd40d7bbb7e8197ad3b888ecdaaf6e6a737ea
oncefirefly <oncefirefly@gmail.com>
2023-08-02 21:02:09 +0300

    feat: Footer component added

---
+++ components/DefaultFooter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="w-full px-10 xl:px-0 bg-slate-700 mt-auto">
+    <small class="max-w-7xl h-14 mx-auto 0 flex items-center text-white f">
+      QTIM-test Blog
+    </small>
+  </footer>
+</template>
+
+<style></style>
