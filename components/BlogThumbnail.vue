0dd2a735a34e8ff7de4e8c3e00679798b3616183
oncefirefly <oncefirefly@gmail.com>
2023-08-03 20:49:36 +0300

    fix: type/null checks

--- components/BlogThumbnail.vue
+++ components/BlogThumbnail.vue
@@ -5,7 +5,7 @@ import { Post } from "@/types/index";
 
 // props
 const props = defineProps<{
-  post: Post | null;
+  post: Post;
 }>();
 
 // composables
@@ -13,7 +13,7 @@ const router = useRouter();
 
 // methods
 const handleBlogClick = () => {
-  router.push({ path: `/${props?.post?.id}` });
+  router.push({ path: `/${props.post.id}` });
 };
 </script>
 
@@ -22,28 +22,27 @@ const handleBlogClick = () => {
     <div
       class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm flex flex-col h-full"
     >
-      <div to="" class="w-full min-h-24 h-1/3 flex items-center justify-center">
+      <div class="w-full min-h-24 h-1/3 flex items-center justify-center">
         <img
           class="rounded-t-lg w-full h-full"
-          :src="props?.post?.image"
-          :alt="props?.post?.image"
+          :src="props.post.image"
+          :alt="props.post.image"
         />
       </div>
       <div class="p-5 flex flex-col h-full">
         <p class="text-xs text-gray-700 mb-3">
-          {{ formatDate(props?.post?.createdAt) }}
+          {{ formatDate(props.post.createdAt) }}
         </p>
         <div href="#">
           <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">
-            {{ props?.post?.title }}
+            {{ props.post.title }}
           </h5>
         </div>
         <p class="font-normal text-gray-700 mb-3">
-          {{ props?.post?.preview }}
+          {{ props.post.preview }}
         </p>
         <button
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 w-24 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center mt-auto"
-          href="#"
         >
           Read more
         </button>
