4bf419385be3b66595cb617b6dd5ab207e5406fd
oncefirefly <oncefirefly@gmail.com>
2023-08-03 19:09:37 +0300

    feat: Blog Details page added

---
+++ pages/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Post } from "@/types/index";
+
+const route = useRoute();
+
+const { data: post } = await useFetch<Post>(
+  `https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/${route.params.id}`
+);
+</script>
+
+<template>
+  <BlogDetails :post="post" />
+</template>
